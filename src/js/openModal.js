8198b6791a635a9bb1030a7e01cfe464edb4b464
Anastasiia Yarmilko <anastasiia.yarm@gmail.com>
2021-10-09 04:55:45 +0300

    Adds modal opening and closing functions

---
+++ src/js/openModal.js
@@ -0,0 +1,23 @@
+const refs = {
+    footerButton: document.querySelector( '.footer-contacts-btn' ),
+    popupCloseButton: document.querySelector( '.popup-btn' ),
+    modalCloseButton: document.querySelector( '.modal-btn' ),
+    modalBackdrop: document.querySelector( '.modal-backdrop' ),
+    popupBackdrop: document.querySelector( '.popup-backdrop' ),
+    input: document.querySelectorAll( 'input' ),
+};
+
+
+const openModal = () => {
+    refs.modalBackdrop.classList.remove( 'is-hidden' );
+    refs.input.forEach(function (inp){
+        inp.value = ''
+    });
+};
+const closePopup = () => refs.popupBackdrop.classList.add( 'is-hidden' );
+const closeModal = () => refs.modalBackdrop.classList.add( 'is-hidden' );
+
+
+refs.footerButton.addEventListener( 'click', openModal );
+refs.popupCloseButton.addEventListener( 'click', closePopup );
+refs.modalCloseButton.addEventListener( 'click', closeModal );
